b1bdb2d3d8374415108493b5b8274f7588bdadec
Pranav g kashyap <pkgnov03@gmail.com>
2026-05-23 12:20:59 +0530

    added the mapper for itellegence service

--- intellegence-service/src/main/java/com/micro/intellegence_service/mapper/ChatMapper.java
+++ intellegence-service/src/main/java/com/micro/intellegence_service/mapper/ChatMapper.java
@@ -2,9 +2,9 @@ package com.micro.intellegence_service.mapper;
 
 
 
+import com.micro.intellegence_service.dto.ChatResponse;
 import com.micro.intellegence_service.entity.ChatMessage;
 import org.mapstruct.Mapper;
-import org.springframework.ai.chat.model.ChatResponse;
 
 import java.util.List;
 
